c4d3d3770f3927d371e107cb4edb4e6003e58ed8
AndresFernandoGarcia <andresgercie@gmail.com>
2025-05-22 22:23:50 +0800

    Bug fixes on gold feature processing table

--- utils/data_processing_gold_table.py
+++ utils/data_processing_gold_table.py
@@ -1,17 +1,7 @@
 import os
-import glob
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-import random
-from datetime import datetime, timedelta
-from dateutil.relativedelta import relativedelta
-import pyspark.sql.functions as F
 from pyspark.ml.feature import StringIndexer # for encoding
-
 from pyspark.sql.functions import col
-from pyspark.sql.types import StringType, IntegerType, FloatType, DateType
-
+from pyspark.sql.types import IntegerType
 
 def process_labels_gold_table(spark):
     
@@ -28,31 +18,43 @@ def process_labels_gold_table(spark):
     for file in csv_files:
         file_path = os.path.join(silver_directory, file)
         base_name = os.path.splitext(file)[0]
-
         df = spark.read.csv(file_path, header=True, inferSchema=True)
         print(f"  - Loaded {file} with {df.count()} rows")
         
+        print("Feature Store currently working with file: ", base_name)
         if(base_name == "features_attributes"):
             df_attr = feature_encode(df, ["Occupation"])
+            df_attr = df_attr.drop("snapshot_date")
 
         elif(base_name == "features_financials"):
             df_finance = feature_encode(df, ["Credit_Mix", "Payment_of_Min_Amount", "Payment_Behaviour"])
 
         elif(base_name == "feature_clickstream_last"):
             df_click = df
+            df_click = df.drop("snapshot_date")
     # Join tables into one
 
     df_combined = df_finance.join(df_attr, on="Customer_ID", how="outer")
     df_combined = df_combined.join(df_click, on="Customer_ID", how="outer")
+
+    # Inserting snapshot_date after Customer_ID
+    cols = df_combined.columns
+    cols.remove("snapshot_date")
+
+    # Insert After Customer_ID
+    cols.insert(cols.index("Customer_ID") + 1, "snapshot_date")
+
+    # Reorder the DataFrame 
+    df_combined = df_combined.select(cols)
     
     # Save gold table - IRL connect to database to write
     partition_name = "feature_store" + '.csv'
     output_file = os.path.join(gold_directory, partition_name)
     df_combined.toPandas().to_csv(output_file, index=False)
     print('saved to:', output_file)
-    
     return df
 
+# numerical encoding values
 def feature_encode(df, column):
     for item in column: 
         outputc = item + "_Encoded"
